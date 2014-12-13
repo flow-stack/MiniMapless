7d9b5e50b4211be0a917a178a65a47907566a0a9
Sebastian Sastre <sebastian@flowingconcept.com>
2014-12-13 18:45:17 -0200

    initial upload

---
+++ devel.js
@@ -0,0 +1,9 @@
+define([
+    'amber/devel',
+    './deploy',
+    // --- packages used only during development begin here ---
+    'amber-minimapless/Minimapless-Tests'
+    // --- packages used only during development end here ---
+], function (amber) {
+    return amber;
+});
